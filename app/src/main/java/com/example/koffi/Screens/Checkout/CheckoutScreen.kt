a76448588169b6a0be6f86a02683ba5c9edb03f2
TanishqBhadula <tanishqbhadula@gmail.com>
2026-04-20 23:31:32 +0530

    Checkout Screen Setup

---
+++ app/src/main/java/com/example/koffi/Screens/Checkout/CheckoutScreen.kt
@@ -0,0 +1,45 @@
+package com.example.koffi.Screens.Checkout
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.koffi.ui.theme.bgWhite
+
+@Preview
+@Composable
+fun CheckoutScreen() {
+    var scrollState = rememberScrollState()
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(bgWhite)
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .background(bgWhite)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(6.dp)
+                    .background(bgWhite)
+                    .verticalScroll(scrollState)
+            ) {
+
+            }
+        }
+    }
+}
